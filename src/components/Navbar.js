ff3c9158b9ecb0ac181f73481e6707a802a3f7ee
Simran Kota <simkota@uw.edu>
2019-03-12 19:55:04 -0700

    just added some style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,37 +16,76 @@ import {
 
 
 export class Nav extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+
     render(){
         return(
-            <nav className="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/">MoviePicks</a>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                            aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                    <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Watch Later</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">My Profile</a>
-                        </li>
-                    </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                        <a href="/search/" className="btn btn-outline-light my-2 my-sm-0" type="submit"  onClick={this.handleClick}>Search</a>
+            // <nav className="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+            //     <div className="container-fluid">
+            //         <a className="navbar-brand" href="/">MoviePicks</a>
+            //             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+            //                 aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+            //                 <span className="navbar-toggler-icon"></span>
+            //             </button>
+            //         <div className="collapse navbar-collapse" id="navbarResponsive">
+            //         <ul className="navbar-nav ml-auto">
+            //             <li className="nav-item active">
+            //                 <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+            //             </li>
+            //             <li className="nav-item">
+            //                 <a className="nav-link" href="#">About</a>
+            //             </li>
+            //             <li className="nav-item">
+            //                 <a className="nav-link" href="#">Watch Later</a>
+            //             </li>
+            //             <li className="nav-item">
+            //                 <a className="nav-link" href="#">My Profile</a>
+            //             </li>
+            //         </ul>
+            //         <form className="form-inline my-2 my-lg-0">
+            //             <a href="/search/" className="btn btn-outline-light my-2 my-sm-0" type="submit"  onClick={this.handleClick}>Search</a>
                         
-                    </form>
-                    </div>
-                    </div>
-                </nav>
+            //         </form>
+            //         </div>
+            //         </div>
+            //     </nav>
+            <Navbar color="dark" className="navbar-dark" expand="md">
+                <NavbarBrand href="/home">MoviePicks</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <NavStrap className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/home">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#">About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/myprofile">Watch Later</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/search/">Search</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/">Log Out</NavLink>
+                        </NavItem>
+                    </NavStrap>
+                </Collapse>
+            </Navbar>
             );
         }
     }
